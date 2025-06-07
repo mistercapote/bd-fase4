14e2c61551c47c7c940e416685576e7f94a83e14
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2025-06-07 12:04:32 -0300

    add cidade.html, estado.html, edicao.html

--- buttons/urls.py
+++ buttons/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
     path('inserir/<str:tabela>/', views.inserir, name="inserir"),
-    path('atualizar/<str:tabela>/', views.atualizar, name="atualizar"),
-    path('listar/<str:tabela>/', views.listar, name="listar"),
-    path('deletar/<str:tabela>/', views.deletar, name="deletar")
+    # path('atualizar/<str:tabela>/', views.atualizar, name="atualizar"),
+    # path('listar/<str:tabela>/', views.listar, name="listar"),
+    # path('deletar/<str:tabela>/', views.deletar, name="deletar")
 ]
 
