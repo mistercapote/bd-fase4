5e954ea2b58436f772b644130e6f5a3f206c4768
mistercapote <luizbravin0@gmail.com>
2025-06-07 13:34:29 -0300

    Inicio da listagem

--- buttons/urls.py
+++ buttons/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
-    path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario")
-    # path('listar/<str:tabela>/', views.listar, name="listar"),
+    path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario"),
+    path('listar/<str:tabela>/', views.listar, name='listar')
     # path('deletar/<str:tabela>/', views.deletar, name="deletar")
 ]
 
--- inserir/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InserirConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inserir'
