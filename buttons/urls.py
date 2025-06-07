cd6495861325f436641e89e198381f4365c8afa4
mistercapote <luizbravin0@gmail.com>
2025-06-07 12:35:14 -0300

    Corrigindo essa porra

--- buttons/urls.py
+++ buttons/urls.py
@@ -3,12 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
-<<<<<<< HEAD
-    path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario"),
-=======
-    path('inserir/<str:tabela>/', views.inserir, name="inserir"),
-    # path('atualizar/<str:tabela>/', views.atualizar, name="atualizar"),
->>>>>>> 07864fbb81a40d3ca0d45b09ee04bde2d1503cab
+    path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario")
     # path('listar/<str:tabela>/', views.listar, name="listar"),
     # path('deletar/<str:tabela>/', views.deletar, name="deletar")
 ]
