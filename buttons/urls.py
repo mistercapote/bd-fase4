12026d58ef2731a59700e92e0e40d0c058358135
mistercapote <luizbravin0@gmail.com>
2025-06-08 17:40:35 -0300

    cidades estados

--- buttons/urls.py
+++ buttons/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario"),
     path('listar/<str:tabela>/', views.listar, name='listar'),
     path('deletar/<str:tabela>/', views.deletar, name="deletar"),
-    path('cidades-por-estado/<int:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
+    path('cidades-por-estado/<str:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
 ]
 
