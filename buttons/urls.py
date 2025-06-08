b1d2951b2f620fdd678ec0dcc8bddbe91f0955dc
mistercapote <luizleb70@gmail.com>
2025-06-08 16:20:04 -0300

    deletar

--- buttons/urls.py
+++ buttons/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
     path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario"),
-    path('listar/<str:tabela>/', views.listar, name='listar')
-    # path('deletar/<str:tabela>/', views.deletar, name="deletar")
+    path('listar/<str:tabela>/', views.listar, name='listar'),
+    path('deletar/<str:tabela>/', views.deletar, name="deletar")
 ]
 
