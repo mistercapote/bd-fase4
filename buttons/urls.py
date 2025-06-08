232e925220ca162b30316e258c530b5a2a2b1eff
mistercapote <luizbravin0@gmail.com>
2025-06-08 18:53:13 -0300

    mudanca de planos

--- buttons/urls.py
+++ buttons/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
-    path('formulario/<str:acao>/<str:tabela>/', views.formulario, name="formulario"),
+    path('inserir/<str:tabela>/', views.formulario, name="formulario"),
+    path('editar/<str:tabela>/<int:pk>/', views.formulario, name='editar'),
     path('listar/<str:tabela>/', views.listar, name='listar'),
-    path('deletar/<str:tabela>/', views.deletar, name="deletar"),
+    path('deletar/<str:tabela>/<int:pk>/', views.deletar, name='deletar'),
     path('cidades-por-estado/<str:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
 ]
 
