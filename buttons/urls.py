ca7c14bc00b8723d4c568941c1cb30c7d60dda0a
mistercapote <luizleb70@gmail.com>
2025-06-09 00:16:20 -0300

    Nova pagina

--- buttons/urls.py
+++ buttons/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
     path('inserir/<str:tabela>/', views.inserir, name="inserir"),
     path('listar/<str:tabela>/', views.listar, name='listar'),
-    path('editar/<str:tabela>/<int:pk>/', views.editar, name='editar'),
-    path('deletar/<str:tabela>/<int:pk>/', views.deletar, name='deletar'),
+    re_path(r'^editar/(?P<tabela>\w+)(?:/(\d+)){1,2}/$', views.editar, name='editar'),
+    re_path(r'^deletar/(?P<tabela>\w+)(?:/(\d+)){1,2}/$', views.deletar, name='deletar'),
     path('cidades-por-estado/<str:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
 ]
