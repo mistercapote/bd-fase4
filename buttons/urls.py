eb10a309465660c56a6dd61a1fcc4dbe180e95b2
mistercapote <luizleb70@gmail.com>
2025-06-09 15:04:25 -0300

    listar composto

--- buttons/urls.py
+++ buttons/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('inserir/<str:tabela>/', views.inserir, name="inserir"),
     path('listar/<str:tabela>/', views.listar, name='listar'),
     path('editar/<str:tabela>/<str:params>/', views.editar, name='editar'),
+    path('editar/<str:tabela>/<str:params1>/<str:params2>/', views.editar, name='editar'),
     path('deletar/<str:tabela>/<str:params>/', views.deletar, name='deletar'),
+    path('deletar/<str:tabela>/<str:params1>/<str:params2>/', views.deletar, name='deletar'),
     path('cidades-por-estado/<str:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
 ]
