fbbddfe9038222c6831ce480920cbec48f95f22c
mistercapote <luizleb70@gmail.com>
2025-06-09 12:14:58 -0300

    edicao e remocao usuario

--- buttons/urls.py
+++ buttons/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.iniciar, name="iniciar"),
     path('inserir/<str:tabela>/', views.inserir, name="inserir"),
     path('listar/<str:tabela>/', views.listar, name='listar'),
-    re_path(r'^editar/(?P<tabela>\w+)(?:/(\d+)){1,2}/$', views.editar, name='editar'),
-    re_path(r'^deletar/(?P<tabela>\w+)(?:/(\d+)){1,2}/$', views.deletar, name='deletar'),
+    path('editar/<str:tabela>/<str:params>/', views.editar, name='editar'),
+    path('deletar/<str:tabela>/<str:params>/', views.deletar, name='deletar'),
     path('cidades-por-estado/<str:estado_id>/', views.cidades_por_estado, name='cidades_por_estado')
 ]
