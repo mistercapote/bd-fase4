e928286a9bd4fe3e2cbba7f8d8ca6b0bbc7e63b8
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2025-06-09 17:02:55 -0300

    a

--- buttons/views.py
+++ buttons/views.py
@@ -309,17 +309,17 @@ def inserir(request, tabela):
             query = "INSERT INTO usravaliaaut (notaautor, autorid, usuarioid) VALUES (%s, %s, %s);"
             params = [notaautor, autorid, usuarioid]
             mensagem_sucesso = f"Avaliação do autor inserida com sucesso!"
-        elif tabela == "usurseguusr":
+        elif tabela == "usrsegueusr":
             datasegusr = request.POST.get('datasegusr')
             seguidorid = request.POST.get('seguidorid')
             seguidoid = request.POST.get('seguidoid')
             
             # Verificar se o relacionamento já existe
             with connection.cursor() as cursor:
-                cursor.execute("SELECT 1 FROM usurseguusr WHERE seguidorid = %s AND seguidoid = %s", [seguidorid, seguidoid])
+                cursor.execute("SELECT 1 FROM usrsegueusr WHERE seguidorid = %s AND seguidoid = %s", [seguidorid, seguidoid])
                 if cursor.fetchone():
                     return HttpResponseBadRequest(f"Erro: Este relacionamento seguidor-seguido já existe.")
-            query = "INSERT INTO usurseguusr (datasegusr, seguidorid, seguidoid) VALUES (%s, %s, %s);"
+            query = "INSERT INTO usrsegueusr (datasegusr, seguidorid, seguidoid) VALUES (%s, %s, %s);"
             params = [datasegusr, seguidorid, seguidoid]
             mensagem_sucesso = f"Usuário começou a seguir outro usuário com sucesso!"
         else:
@@ -329,7 +329,7 @@ def inserir(request, tabela):
             try:
                 with connection.cursor() as cursor:
                     cursor.execute(query, params)
-                return HttpResponse(mensagem_sucesso)
+                return redirect('/')
             except Exception as e:
                 return HttpResponseBadRequest(f"Erro ao inserir: {str(e)}")
 
@@ -366,45 +366,45 @@ def editar(request, tabela, params, params2=None):
         
     elif request.method == "POST":
         query = None
-        params = []
+        parametross = []
         mensagem_sucesso = ""
 
         if tabela == "estado":
-            estadoid = request.POST.get('estadoid')
+            estadoid = params[0]
             nomeestado = request.POST.get('nomeestado')
             query = "UPDATE estado SET nomeestado = %s WHERE estadoid = %s;"
-            params = [nomeestado, estadoid]
+            parametross = [nomeestado, estadoid]
             mensagem_sucesso = f"Estado {nomeestado} atualizado com sucesso!"
         elif tabela == "livro":
-            livroid = request.POST.get('livroid')
+            livroid = params[0]
             nomelivro = request.POST.get('nomelivro')
             query = "UPDATE livro SET nomelivro = %s WHERE livroid = %s;"
-            params = [nomelivro, livroid]
+            parametross = [nomelivro, livroid]
             mensagem_sucesso = f"Livro {nomelivro} atualizado com sucesso!"
         elif tabela == "editora":
-            editoraid = request.POST.get('editoraid')
+            editoraid = params[0]
             nomeeditora = request.POST.get('nomeeditora')
             siteoficial = request.POST.get('siteoficial')
             descricaoeditora = request.POST.get('descricaoeditora')
             query = ("UPDATE editora SET nomeeditora = %s, siteoficial = %s, descricaoeditora = %s "
                         "WHERE editoraid = %s;")
-            params = [nomeeditora, siteoficial, descricaoeditora, editoraid]
+            parametross = [nomeeditora, siteoficial, descricaoeditora, editoraid]
             mensagem_sucesso = f"Editora {nomeeditora} atualizada com sucesso!"
         elif tabela == "autor":
-            autorid = request.POST.get('autorid')
+            autorid = params[0]
             nomeautor = request.POST.get('nomeautor')
             descricaoautor = request.POST.get('descricaoautor')
             query = "UPDATE autor SET nomeautor = %s, descricaoautor = %s WHERE autorid = %s;"
-            params = [nomeautor, descricaoautor, autorid]
+            parametross = [nomeautor, descricaoautor, autorid]
             mensagem_sucesso = f"Autor {nomeautor} atualizado com sucesso!"
         elif tabela == "livroaut":
-            livroid = request.POST.get('livroid')
-            autorid = request.POST.get('autorid')
+            livroid = params[0]
+            autorid = params[1]
             query = "UPDATE livroaut SET autorid = %s WHERE livroid = %s;"
-            params = [autorid, livroid]
+            parametross = [autorid, livroid]
             mensagem_sucesso = f"Relacionamento Livro-Autor atualizado com sucesso!"
         elif tabela == "edicao":
-            edicaoid = request.POST.get('edicaoid')
+            edicaoid = params[0]
             anopublicacao = request.POST.get('anopublicacao')
             isbn13 = request.POST.get('isbn13')
             isbn10 = request.POST.get('isbn10')
@@ -414,25 +414,25 @@ def editar(request, tabela, params, params2=None):
             livroid = request.POST.get('livroid')
             editoraid = request.POST.get('editoraid')
             query = ("UPDATE edicao SET anopublicacao = %s, isbn13 = %s, isbn10 = %s, idioma = %s, numpaginas = %s, descricaoedicao = %s, livroid = %s, editoraid = %s WHERE edicaoid = %s;")
-            params = [anopublicacao, isbn13, isbn10, idioma, numpaginas, descricaoedicao, livroid, editoraid, edicaoid]
+            parametross = [anopublicacao, isbn13, isbn10, idioma, numpaginas, descricaoedicao, livroid, editoraid, edicaoid]
             mensagem_sucesso = f"Edição {edicaoid} atualizada com sucesso!"
         elif tabela == "avaliacao":
-            avalid = request.POST.get('avalid')
+            avalid = params[0]
             tituloaval = request.POST.get('tituloaval')
             corpoaval = request.POST.get('corpoaval')
             avalid = int(avalid) if avalid else None
             query = "UPDATE avaliacao SET tituloaval = %s, corpoaval = %s WHERE avalid = %s;"
-            params = [tituloaval, corpoaval, avalid]
+            parametross = [tituloaval, corpoaval, avalid]
             mensagem_sucesso = f"Avaliação {tituloaval} atualizada com sucesso!"
         elif tabela == "cidade":
-            cidadeid = request.POST.get('cidadeid')
+            cidadeid = params[0]
             nomecidade = request.POST.get('nomecidade')
             estadoid = request.POST.get('estadoid')
             query = "UPDATE cidade SET nomecidade = %s, estadoid = %s WHERE cidadeid = %s;"
-            params = [nomecidade, estadoid, cidadeid]
+            parametross = [nomecidade, estadoid, cidadeid]
             mensagem_sucesso = f"Cidade {nomecidade} atualizada com sucesso!"
         elif tabela == "usuario":
-            usuarioid = request.POST.get('usuarioid')
+            usuarioid = params[0]
             nomeusuario = request.POST.get('nomeusuario')
             senha = request.POST.get('senha')
             emailusuario = request.POST.get('emailusuario')
@@ -443,7 +443,7 @@ def editar(request, tabela, params, params2=None):
             apelido = request.POST.get('apelido')
             cidadeid = request.POST.get('cidadeid')
             query = ("UPDATE usuario SET nomeusuario = %s, senha = %s, emailusuario = %s, datanascimento = %s, genero = %s, fotoperfil = %s, biografia = %s, apelido = %s, cidadeid = %s WHERE usuarioid = %s;")
-            params = [nomeusuario, senha, emailusuario, datanascimento, genero, fotoperfil, biografia, apelido, cidadeid, usuarioid]
+            parametross = [nomeusuario, senha, emailusuario, datanascimento, genero, fotoperfil, biografia, apelido, cidadeid, usuarioid]
             mensagem_sucesso = f"Usuário {nomeusuario} atualizado com sucesso!"
             
             # Verificar se o email já está sendo usado por OUTRO usuário
@@ -456,44 +456,43 @@ def editar(request, tabela, params, params2=None):
             query = ("UPDATE usuario SET nomeusuario = %s, senha = %s, emailusuario = %s, "
                     "datanascimento = %s, genero = %s, fotoperfil = %s, biografia = %s, "
                     "apelido = %s, cidadeid = %s WHERE usuarioid = %s;")
-            params = [nomeusuario, senha, emailusuario, datanascimento, genero, 
+            parametross = [nomeusuario, senha, emailusuario, datanascimento, genero, 
                      fotoperfil, biografia, apelido, cidadeid, usuarioid]
         
         elif tabela == "usrlelivro":
             statusleitura = request.POST.get('statusleitura')
             notalivro = request.POST.get('notalivro')
-            usuarioid = request.POST.get('usuarioid')
-            livroid = request.POST.get('livroid')
+            usuarioid = params[0]
+            livroid = params[1]
             avalid = request.POST.get('avalid')
             query = ("UPDATE usrlelivro SET statusleitura = %s, notalivro = %s, usuarioid = %s, livroid = %s, avalid = %s WHERE usuarioid = %s AND livroid = %s;")
-            params = [statusleitura, notalivro, usuarioid, livroid, avalid, usuarioid, livroid]
+            parametross = [statusleitura, notalivro, usuarioid, livroid, avalid, usuarioid, livroid]
             mensagem_sucesso = f"Status de leitura atualizado com sucesso!"
         elif tabela == "usrsegueaut":
             datasegaut = request.POST.get('datasegaut')
-            usuarioid = request.POST.get('usuarioid')
-            autorid = request.POST.get('autorid')
+            usuarioid = params[0]
+            autorid = params[0]
             query = "UPDATE usrsegueaut SET datasegaut = %s WHERE usuarioid = %s AND autorid = %s;"
-            params = [datasegaut, usuarioid, autorid]
+            parametross = [datasegaut, usuarioid, autorid]
             mensagem_sucesso = f"Atualização de seguimento do autor realizada com sucesso!"
         elif tabela == "usravaliaaut":
             notaautor = request.POST.get('notaautor')
-            autorid = request.POST.get('autorid')
-            usuarioid = request.POST.get('usuarioid')
+            usuarioid = params[0]
+            autorid = params[1]
             query = "UPDATE usravaliaaut SET notaautor = %s WHERE autorid = %s AND usuarioid = %s;"
-            params = [notaautor, autorid, usuarioid]
+            parametross = [notaautor, autorid, usuarioid]
             mensagem_sucesso = f"Avaliação do autor atualizada com sucesso!"
-        elif tabela == "usurseguusr":
+        elif tabela == "usrsegueusr":
             datasegusr = request.POST.get('datasegusr')
-            seguidorid = request.POST.get('seguidorid')
-            seguidoid = request.POST.get('seguidoid')
-            query = "UPDATE usurseguusr SET datasegusr = %s WHERE seguidorid = %s AND seguidoid = %s;"
-            params = [datasegusr, seguidorid, seguidoid]
+            seguidorid = params[0]
+            seguidoid = params[1]
+            query = "UPDATE usrsegueusr SET datasegusr = %s WHERE seguidorid = %s AND seguidoid = %s;"
+            parametross = [datasegusr, seguidorid, seguidoid]
             mensagem_sucesso = f"Atualização de seguimento de usuário realizada com sucesso!"
         else:
             return HttpResponseBadRequest("Tabela inválida.")
 
         if query:
             with connection.cursor() as cursor:
-                cursor.execute(query, params)
+                cursor.execute(query, parametross)
             return HttpResponse(mensagem_sucesso)
-   
